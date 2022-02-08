a66f5792d0dd094442fcfd552672a19e3a723300
Chibuzo Chiwike <chibuzo.m.chiwike.22@dartmouth.edu>
2022-02-07 20:27:21 -0500

    logs

--- database/create_tables.js
+++ database/create_tables.js
@@ -26,4 +26,34 @@ client
         console.log(`client failed to connect with error: ${error}`)
     });
 
-console.log('finished create');
+// SQL commands that will be run
+// const create_user = `CREATE ROLE ${process.env.PGUSER} WITH LOGIN PASSWORD '${process.env.PGPASSWORD}';`
+// const give_role = `ALTER ROLE ${process.env.PGUSER} CREATEDB;`
+const create_users = `CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY, name TEXT NOT NULL, email TEXT NOT NULL UNIQUE, password_hash TEXT NOT NULL, is_admin BOOLEAN NOT NULL, eth_address TEXT NOT NULL);`
+const create_auth = `CREATE TABLE IF NOT EXISTS auth (token TEXT PRIMARY KEY, id integer REFERENCES users ON DELETE CASCADE, timestamp TIMESTAMPTZ NOT NULL DEFAULT (NOW() + interval '336 hours'));`
+
+const execute = async (query) => {
+    try {
+        await client.query(query);  // sends queries
+        return true;
+    } catch (error) {
+        console.error(error.stack);
+        return false;
+    }
+};
+
+// users table
+execute(create_users).then(result => {
+    if (result) {
+        console.log('users table created.');
+    }
+});
+
+// auth table
+execute(create_auth).then(result => {
+    if (result) {
+        console.log('auth table created.');
+    }
+});
+
+console.log('finished create'); 
